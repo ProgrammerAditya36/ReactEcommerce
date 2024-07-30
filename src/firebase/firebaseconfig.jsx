51765e01925a8da099bfd430471d68b0a67d9a4a
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-07-31 01:53:47 +0530

    Auto commit

--- src/firebase/firebaseconfig.jsx
+++ src/firebase/firebaseconfig.jsx
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAPE5yVYta1P1tZDrLqqCmbdz3jQCdXncY",
-    authDomain: "ecommerce-a5864.firebaseapp.com",
-    projectId: "ecommerce-a5864",
-    storageBucket: "ecommerce-a5864.appspot.com",
-    messagingSenderId: "665870264924",
-    appId: "1:665870264924:web:8e5d69726290c7cc051499",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 export const fireDb = getFirestore(app);
 export const auth = getAuth(app);
